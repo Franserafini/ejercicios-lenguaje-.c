e3c5c17fc4dc65296d143da7d8ae4f846d838a2c
Francisco Serafini <86386807+Franserafini@users.noreply.github.com>
2021-07-30 11:05:58 -0300

    Add files via upload

---
+++ Calcular el promedio de las notas ingresadas por el usuario (While, centinela,etc).c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+//(3)
+//Calcular el promedio de las notas ingresadas por el usuario
+//Para finalizar ingresar -1
+
+
+
+
+int main(void) {
+	
+	float promedio, suma = 0;
+	int nota, contador_notas = 0;
+	int repetir = 1;
+	
+	while(repetir){
+		printf("Ingrese la nota:");
+		scanf("%d", &nota);
+	
+		
+		if( nota == -1){
+			repetir = 0;
+			
+		} else {
+			suma += nota;
+			contador_notas = contador_notas + 1;
+		}
+	}
+	promedio = suma / contador_notas;
+	printf("El promedio de las notas ingresadas es: %f\n", promedio);
+	
+	
+	
+	
+	
+	return 0;
+}
+
