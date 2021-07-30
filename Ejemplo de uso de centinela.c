8bae08373a8cf42b2840c243091aef1ae060a85a
Francisco Serafini <86386807+Franserafini@users.noreply.github.com>
2021-07-30 10:51:34 -0300

    Add files via upload

---
+++ Ejemplo de uso de centinela.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	int centinela = 0;
+	int num;
+	
+	while (centinela == 0){
+		printf("Ingrese el numero 23:");
+		scanf ("%d", &num);
+		
+		if (num ==23)
+			centinela = 1;
+		else 
+			printf ("Numero incorrecto\n");
+		
+		
+	}
+	printf ("Usted ingreso el %d!\n", num);
+	
+	
+	
+	return 0;
+}
+
---
+++ Switch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	unsigned int nota;
+	
+	nota = 4;
+	
+	switch (nota){
+		
+	case 10:
+	case 9:
+	case 8:
+		printf("Promocionado\n");
+	
+		break;
+	case 7:
+	case 6:
+		printf("Aprobado\n");
+		break;
+	default:
+		printf("Libre");
+		break;
+		}
+return 0;
+}
