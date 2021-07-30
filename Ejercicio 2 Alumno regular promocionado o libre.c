4e7c4a5883e50d0d0c39c29676f918e9edb5c82e
Francisco Serafini <86386807+Franserafini@users.noreply.github.com>
2021-07-30 10:42:48 -0300

    Add files via upload

---
+++ Ejercicio 2 Alumno regular promocionado o libre.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	unsigned char nota; // 1 byte
+	
+	printf ("Ingrese la nota:");
+		scanf("%hhu", &nota);
+	
+	if (nota < 6) {
+		printf("El estado del alumno es libre");
+	
+	} else {
+		if (nota >= 8){
+			printf(" El estado del alumno es promocionado");
+		
+		
+		} else {
+			if (nota >= 6){
+				printf ("El estado del alumno es aprobado");
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
